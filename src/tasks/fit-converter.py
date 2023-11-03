4c5f5b936e120371016b0bd05aab0d29d9f1ece6
Victor Mari Rodriguez <vrodriguez@tmforum.org>
2023-11-03 22:02:27 +0100

    Adding converter

--- src/tasks/fit-converter.py
+++ src/tasks/fit-converter.py
@@ -1,41 +1,56 @@
 import subprocess
 import shutil
 from pathlib import Path
-
+import json
 
 CONVERTER = Path(__file__).parents[0] / "tools" / "FitCSVTool.jar"
 
 def convert_fit_file(input_file, output_file):
+    print("Converting {} to {}".format(input_file, output_file))
+    output = {
+        "code": None,
+        "stdout": None,
+        "stderr": None
+    }
+
     command = [
         "java",
         "-jar",
         str(CONVERTER),
-        input_file,
+        "-b",
+        str(input_file),
+        str(output_file)
     ]
     try:
         result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        data = {
-            "code": result.returncode,
-            "stdout": result.stdout.decode("utf-8"),
-            "stderr": result.stderr.decode("utf-8")
-        }
-        return data
+        output["code"] =  result.returncode
+        output["stdout"] = result.stdout.decode("utf-8")
+        output["stderr"] = result.stderr.decode("utf-8")
+
 
     except FileNotFoundError:
         print("FitCSVTool.jar not found. Please download")
-
+        output["stdout"] =  "FitCSVTool.jar not found. Please download",
 
     except subprocess.CalledProcessError as e:
         print(e.stderr.decode("utf-8"))
-        return None
+        output["code"] =  e.returncode
+        output["stdout"] = e.stdout.decode("utf-8")
+        output["stderr"] = e.stderr.decode("utf-8")
 
+    return output
 
 
 def main(args):
     input_file = Path(__file__).parents[1] / "data" / "illetes.fit"
-    output_file = Path(__file__).parents[2] / "data" / "illetes.csv"
+    output_file = Path(__file__).parents[1] / "data" / "illetes.csv"
     result = convert_fit_file(input_file, output_file)
-    print(result)
+
+    print(result["code"])
+    print(result["stderr"])
+    print(result["stdout"])
+
+
     return 0
 
 
