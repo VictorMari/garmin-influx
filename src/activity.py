e1c5c822383bfeba76456a9de60c586122baa6a8
Victor Mari Rodriguez <rodriguezvi@microsoft.com>
2024-11-12 22:29:25 +0000

    Improving parser

--- src/activity.py
+++ src/activity.py
@@ -1,5 +1,11 @@
-from garmin_fit_sdk import Decoder, Stream
+from garmin_fit_sdk import (
+    Decoder, 
+    Stream,
+    Profile,
+)
 from pathlib import Path
+import json
+
 
 
 PROJECT = Path(__file__).parents[1]
@@ -8,6 +14,24 @@ ACTIVITIES = PROJECT / "bin" / "Activities"
 class SwimmingActivity:
     def __main__(self, messages):
         pass
+
+class ActivityParser:
+    def __init__(self):
+        self.sessions = {}
+        self.messageTypes = {}
+        self.file_id = None
+        self.message_types = set()
+        self.message_names = set()
+
+    def parse(self, messages):
+        for message_type in messages.keys():
+            self.message_types.add(message_type)
+
+        
+    def get_types(self):
+        return list(self.message_types)
+
+        
 
 
 
@@ -23,7 +47,7 @@ def load_activities():
         "enable_crc_check": True,
         "expand_sub_fields": True,
         "expand_components": True,
-        "convert_types_to_strings": True,
+        #"convert_types_to_strings": True,
         "convert_datetimes_to_dates": True,
         "merge_heart_rates": True,
     }
@@ -32,15 +56,21 @@ def load_activities():
         "Swimming": SwimmingActivity   
     }
 
+    parser = ActivityParser()
+
     for fit_file in ACTIVITIES.glob("*.fit"):
         print(f"Processing {fit_file.name}")
         stream = Stream.from_file(fit_file)
         decoder = Decoder(stream)
         #is_well = decoder.check_integrity()
         messages, errors = decoder.read(**options)
-        process_errors(errors)
-
-
+        if errors:
+            process_errors(errors)
+            return 1
+        parser.parse(messages)
+    
+    with open("groups.json", "w+") as f:
+        json.dump(parser.get_types(), f, indent=4)  
 
 
 def main():
